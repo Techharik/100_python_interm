9ab696deb4e0d56e52f74cbf9288bdc9a2c295e3
Techharik <khariprasath30@gmail.com>
2024-04-15 17:42:11 +0530

    unlimited karags ** arguments with funtion

--- day27/main.py
+++ day27/main.py
@@ -1,4 +1,30 @@
 #GUI work with tkinker.
+
+# ? * unlimited arguments
+
+# def add(*args):
+# #   args gives as a tupple
+#   count = 0
+#   for n in args:
+#      count = count+n
+#   return count
+
+# result = add(1,23,4)
+# print(result)
+
+# ? ** kwargs
+
+# we can pass a keyword argument and it returns  a dictionary.add()
+
+def add(**kwargs):
+    for key, value in kwargs.items():
+        print(key)
+          
+
+
+add(add=3,sub=4)
+
+
 import tkinter as tk
 
 window = tk.Tk()
@@ -13,16 +39,8 @@ my_lable = tk.Label(text='I am a robot',font=('Arial',34,"bold"))
 my_lable.pack() # pack is important to show
 
 
- 
-def add(*args):
-#   args gives as a tupple
-  count = 0
-  for n in args:
-     count = count+n
-  return count
 
-result = add(1,23,4)
-print(result)
+
 
 
 
